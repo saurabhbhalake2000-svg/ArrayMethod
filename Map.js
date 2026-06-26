9c6b7deac173541128da0318bcfceb8923da3872
Sagar <saurabhbhalake2000@gmail.com>
2026-06-26 10:57:02 +0530

    chnagess

--- Map.js
+++ Map.js
@@ -1,6 +1,6 @@
 var data =[10,20,30,40,50];
 var newArray = data.map(function (element,index){
-    var result = element * 100;
-    return result;
+    var a = element * 100;
+    return a   ;
 });
 console.log(newArray);
