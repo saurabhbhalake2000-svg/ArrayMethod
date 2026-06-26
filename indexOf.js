8e6c0b8e6e0ffef0ba47be2f41f0b24e005e59a2
Sagar <saurabhbhalake2000@gmail.com>
2026-06-26 10:59:19 +0530

    change indexOf

--- indexOf.js
+++ indexOf.js
@@ -1,16 +1,16 @@
-var data = [10,20,true, "sagar",10, { city:"pune" }, [1,2,3]];
+var rest = [10,20,true, "sagar",10, { city:"pune" }, [1,2,3]];
 
 //slice :[20,true,"sagar"] : array.slice(startIndex,lastIndex+1)
-var subArray=data.slice(1,3+1);   //var subArray=data.slice(1);  want to access all
+var subArray=rest.slice(1,3+1);   //var subArray=rest.slice(1);  want to access all
 console.log(subArray);
 
 
 //indexOf :ondexOf will return first encounter element index number 
-var indexNumber = data.indexOf(10);
+var indexNumber = rest.indexOf(10);
 console.log(indexNumber);
 
 //lastIndexOf :lastIndexOf will return last encounter element index number 
-var indexNumber = data.lastIndexOf(10);
+var indexNumber = rest.lastIndexOf(10);
 console.log(indexNumber);
 
 
@@ -34,11 +34,11 @@ console.log(indexNumber);
 console.log("-------------------------")
 console.log()
 
-//console.log(data.length);
+//console.log(rest.length);
 //indexOf
-var indexNumber =data.indexOf(10);
+var indexNumber =rest.indexOf(10);
 console.log(indexNumber);
 
 //lastIndexOf
-var indexNumber =data.lastindex(10);
+var indexNumber =rest.lastindex(10);
 console.log(indexNumber);
